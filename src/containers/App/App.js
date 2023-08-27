9b113630bdf712180bb9275770c9385b0d541e6b
Aslan <aslan86@mail.ru>
2023-08-27 08:08:36 +0300

    утилита для работы с API

---
+++ src/containers/App/App.js
@@ -0,0 +1,14 @@
+import styles from './App.module.css'
+
+// getApiResource()
+
+const App = () => {
+  return (
+    <div>
+      <h1 className={styles.header}>Hello</h1>
+    </div>
+  )
+}
+
+export default App
+
