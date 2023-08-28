2b3d6da971544e625ca6d323552f10fcf4e7e4f6
Aslan <aslan86@mail.ru>
2023-08-28 12:51:26 +0300

    Добавил стили

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,3 +1,4 @@
+import styles from './PeoplePage.module.css'
 import { useEffect, useState } from 'react'
 import { getApiResource } from '../../utils/network'
 import { API_PEOPLE } from '../../constants/api'
