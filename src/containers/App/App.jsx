a70b3ffba2fffc73256dd6d9fdaed8ffb3495751
Aslan <aslan86@mail.ru>
2023-08-31 12:41:14 +0300

    подключил react-router-dom

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import PeoplePage from '@containers/PeoplePage/PeoplePage'
+import HomePage from '../HomePage/HomePage'
 import styles from './App.module.css'
 
 const App = () => {
    return (
-      <div>
-         <PeoplePage />
-      </div>
+      <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/people' element={<PeoplePage />} />
+         </Routes>
+      </BrowserRouter>
    )
 }
 
