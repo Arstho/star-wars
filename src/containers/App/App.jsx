2f13490677b6a7fee417b765a799976c0a3fe76b
Aslan <aslan86@mail.ru>
2023-08-31 14:55:41 +0300

    добавил комплонент Header, стилизовал Header и компонент App

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,17 +1,23 @@
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import routesConfig from '@routes/routesConfig'
+import Header from '@components/Header/Header'
 import styles from './App.module.css'
 
 const App = () => {
    return (
       <BrowserRouter>
-         <NavLink to='/' exact>Home</NavLink>
-         <NavLink to='/people' exact>People</NavLink>
-         <Routes>
-            {routesConfig.map(route => (
-               <Route key={route.path} path={route.path} element={route.element} />
-            ))}
-         </Routes>
+         <div className={styles.wrapper}>
+            <Header />
+            <Routes>
+               {routesConfig.map(route => (
+                  <Route
+                     key={route.path}
+                     path={route.path}
+                     element={route.element}
+                  />
+               ))}
+            </Routes>
+         </div>
       </BrowserRouter>
    )
 }
