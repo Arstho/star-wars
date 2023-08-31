364bc9a5bfedafde1d39c045ebdd46c13d0ae781
Aslan <aslan86@mail.ru>
2023-08-31 13:13:07 +0300

    добавил файл routesConfig

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import PeoplePage from '@containers/PeoplePage/PeoplePage'
-import HomePage from '../HomePage/HomePage'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import routesConfig from '@routes/routesConfig'
 import styles from './App.module.css'
 
 const App = () => {
    return (
       <BrowserRouter>
+         <NavLink to='/' exact>Home</NavLink>
+         <NavLink to='/people' exact>People</NavLink>
          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/people' element={<PeoplePage />} />
+            {routesConfig.map(route => (
+               <Route key={route.path} path={route.path} element={route.element} />
+            ))}
          </Routes>
       </BrowserRouter>
    )
