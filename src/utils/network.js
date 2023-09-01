9e48feb5c58515865367ce29047e8776777e1df1
Aslan <aslan86@mail.ru>
2023-09-01 21:12:08 +0300

    реализовал функцию changeHTTP

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,4 +1,20 @@
+import { HTTP, HTTPS } from "@constants/api"
 
+/**
+ * изменяет URL с HTTP на HTTPS
+ * @param {string} url - url для изменения
+ * @returns {string} url - url с HTTPS
+ */
+export const changeHTTP = url => {
+   const result = url ? url.replace(HTTP, HTTPS) : url
+   return result
+}
+
+/**
+ * Отправляет запрос fetch
+ * @param {string} url - url для запроса
+ * @returns {Promise} - Promise с результатом запроса
+ */
 export const getApiResource = async (url) => {
    try {
       const res = await fetch(url)
