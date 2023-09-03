8d5f8b836c901f580a8c660e5b4b8b5fcd485319
Aslan <aslan86@mail.ru>
2023-09-03 19:50:26 +0300

    формирование данных для отображения

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,18 +1,35 @@
 import PropTypes from 'prop-types'
 import { useParams } from 'react-router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getApiResource } from '@utils/network'
 import { API_PERSON } from '../../constants/api'
 
 import styles from './PersonPage.module.css'
+import { withErrorApi } from '../../hoc-helper/withErrorApi'
 
-const PersonPage = () => {
+const PersonPage = ({ setErrorApi }) => {
    const { id } = useParams()
+   const [personInfo, setPersonInfo] = useState(null)
+   const [personName, setPersonName] = useState(null)
 
    useEffect(() => {
       (async () => {
          const res = await getApiResource(`${API_PERSON}/${id}/`)
-         console.log(`${API_PERSON}/${id}/`);
+         if (res) {
+            setPersonInfo([
+               { title: 'Heigth', dat: res.heigth },
+               { title: 'Mass', dat: res.mass },
+               { title: 'Hair color', dat: res.hair_color },
+               { title: 'Skin color', dat: res.skin_color },
+               { title: 'Eye color', dat: res.eye_color },
+               { title: 'Birth year', dat: res.birth_year },
+               { title: 'Gender', dat: res.gender },
+            ])
+            setPersonName(res.name)
+            setErrorApi(false)
+         } else {
+            setErrorApi(true)
+         }
       })()
    }, [])
 
@@ -23,4 +40,8 @@ const PersonPage = () => {
    )
 }
 
-export default PersonPage
+PersonPage.propTypes = {
+   setErrorApi: PropTypes.func
+}
+
+export default withErrorApi(PersonPage)
