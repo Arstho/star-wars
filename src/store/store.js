59733c9692bc52bc8945c31491680b980589f76a
Aslan <aslan86@mail.ru>
2023-09-07 21:48:04 +0300

    настроил devTools

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,11 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import rootReduser from './redusers'
 
-const store = createStore(rootReduser)
+const store = createStore(
+   rootReduser,
+   composeWithDevTools(applyMiddleware(thunk))
+)
 
 export default store
