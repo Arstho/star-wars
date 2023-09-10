092210a8ae54b0f0d4d206930e56ecf0b96845ce
Aslan <aslan86@mail.ru>
2023-09-10 21:54:55 +0300

    API для поиска

--- src/containers/SearchPage/SearchPage.jsx
+++ src/containers/SearchPage/SearchPage.jsx
@@ -1,13 +1,33 @@
 import React, { useState } from 'react'
 import { getApiResource } from '@utils/network';
+import { API_SEARCH } from '@constants/api';
+import { withErrorApi } from '@hoc-helper/withErrorApi';
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
 
 import styles from './SearchPage.module.css';
 
-const SearchPage = () => {
+const SearchPage = ({ setErrorApi }) => {
    const [inputSearchValue, setInputSearchValue] = useState('')
+   const [people, setPeople] = useState([])
 
    const getResponse = async (param) => {
-      const res = await getApiResource()
+      const res = await getApiResource(API_SEARCH + param)
+
+      if (res) {
+         const peopleList = res.results.map(({ name, url }) => {
+            const id = getPeopleId(url)
+            const img = getPeopleImage(id)
+            return {
+               id,
+               name,
+               img
+            }
+         })
+         setPeople(peopleList);
+         setErrorApi(false)
+      } else {
+         setErrorApi(true)
+      }
    }
 
    const handleInputChange = (event) => {
@@ -27,4 +47,4 @@ const SearchPage = () => {
    )
 }
 
-export default SearchPage
+export default withErrorApi(SearchPage)
